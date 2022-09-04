cc8bc04a5b95e2ecda97e652fe8276623c78a1f5
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2022-09-04 18:34:26 -0400

    test: add mailto test

--- tests/mailto.test.ts
+++ tests/mailto.test.ts
@@ -1,7 +1,43 @@
 import { mailto } from "../src/index";
 
 describe("mailto", () => {
-  test("correct output", () => {
-    expect(mailto("test@example.com")).toEqual("mailto:test@example.com");
+  test("only address", () => {
+    expect(mailto("foo@example.com")).toEqual("mailto:foo@example.com");
+  });
+
+  test("cc", () => {
+    expect(
+      mailto("foo@example.com", {
+        cc: ["bar@example.com"],
+      })
+    ).toEqual("mailto:foo@example.com?cc=bar%40example.com");
+  });
+
+  test("bcc", () => {
+    expect(
+      mailto("foo@example.com", {
+        bcc: ["bar@example.com", "baz@example.com"],
+      })
+    ).toEqual("mailto:foo@example.com?bcc=bar%40example.com,baz%40example.com");
+  });
+
+  test("subject", () => {
+    expect(
+      mailto("foo@example.com", {
+        subject: "Do not reply to this email",
+      })
+    ).toEqual(
+      "mailto:foo@example.com?subject=Do%20not%20reply%20to%20this%20email"
+    );
+  });
+
+  test("subject", () => {
+    expect(
+      mailto("foo@example.com", {
+        body: "This is a scam! What is you're credit card number?",
+      })
+    ).toEqual(
+      "mailto:foo@example.com?body=This%20is%20a%20scam!%20What%20is%20you're%20credit%20card%20number%3F"
+    );
   });
 });
