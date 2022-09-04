70792476ca11cd8707f229c9165fc47824546174
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2022-09-04 18:33:49 -0400

    feat: paramify funcs

---
+++ src/global.ts
@@ -0,0 +1,12 @@
+export const paramifyValue = (value: any): string =>
+  Array.isArray(value)
+    ? value.map(encodeURIComponent).join(",")
+    : encodeURIComponent(value);
+
+export const paramify = (rawParams: Record<string, any> = {}): string => {
+  const params = Object.entries(rawParams)
+    .map(([k, v]) => `${k}=${paramifyValue(v)}`)
+    .join("&");
+
+  return (params.length ? "?" : "") + params;
+};
