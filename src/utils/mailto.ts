b81df5cb8b64a7dd0fc095baa89d5335b04515ce
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2022-09-05 00:08:14 -0400

    refactor: add mailto comment and allow multiple addresses

--- src/utils/mailto.ts
+++ src/utils/mailto.ts
@@ -1,11 +1,17 @@
 import { paramify } from "../global";
 
+/**
+ * Generate a link to open the device's default mail client with prefilled `options`
+ * @param address recipient addresses
+ * @param options additional prefilled fields
+ */
 export const mailto = (
-  address: string,
+  addresses: string[],
   options: {
     subject?: string;
     body?: string;
     cc?: string[];
     bcc?: string[];
   } = {}
-): string => encodeURI(`mailto:${address}`) + paramify(options);
+): string =>
+  encodeURI(`mailto:${addresses.map(encodeURI).join(",")}`) + paramify(options);
