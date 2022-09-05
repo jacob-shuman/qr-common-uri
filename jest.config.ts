28d85d61f210a3a9bd9171664545f08eea400f4e
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2022-09-04 22:36:43 -0400

    chore: setup jest watch ignore

--- jest.config.ts
+++ jest.config.ts
@@ -190,7 +190,7 @@ const config: Config = {
   // verbose: undefined,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-  // watchPathIgnorePatterns: [],
+  watchPathIgnorePatterns: ["./dist/*", "./src/index.ts"],
 
   // Whether to use watchman for file crawling
   // watchman: true,
